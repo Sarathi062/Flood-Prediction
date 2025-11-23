4270086e995142e161a3c25d9880359bf2586a2f
YASHRAJ DHAMALE <yashrajdhamale15@gmail.com>
2025-11-24 00:47:01 +0530

    fix: Update 404 page to use JavaScript for redirection and save requested path

--- src/index.js
+++ src/index.js
@@ -5,6 +5,13 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 
+// ⭐ Fix: Apply SPA fallback before rendering
+if (sessionStorage.path) {
+  const path = sessionStorage.path;
+  sessionStorage.removeItem("path");
+  window.history.replaceState(null, null, path);
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
@@ -14,7 +21,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
